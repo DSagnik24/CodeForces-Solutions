342fbbbc179351fb16f7f087f42c59a05ddaa5fd
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-08-06 18:21:40 +0530

    added 50A

---
+++ A_Problems/Bit_282A.java
@@ -0,0 +1,10 @@
+package A_Problems;
+
+import java.util.Scanner;
+
+public class Bit_282A {
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+
+    }
+}
---
+++ A_Problems/DominoPilling_50A.java
@@ -0,0 +1,16 @@
+package A_Problems;
+
+import java.util.Scanner;
+
+public class DominoPilling_50A {
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+
+        int M = in.nextInt();
+        int N = in.nextInt();
+
+        int x = (M*N)/ 2;
+
+        System.out.println(x);
+    }
+}
